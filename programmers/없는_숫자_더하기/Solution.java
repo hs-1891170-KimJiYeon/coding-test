c033bd0dd20fa03a97bef4b9fec17541aa49f5c8
Kim-jiyeon224 <shjh0107@gmail.com>
2021-09-16 23:55:30 +0900

    없는 숫자 더하기 [다른사람]

--- programmers/없는_숫자_더하기/Solution.java
+++ programmers/없는_숫자_더하기/Solution.java
@@ -1,17 +1,21 @@
 class Soultion{
 
     public int solution(int[] numbers) {
-        List<Integer> list = new ArrayList<>();
-        for(int i= 0; i<10; i++){
-            list.add(i);
-        }
-        for(int n : numbers){
-            list.remove(Integer.valueOf(n));
-        }
-        int sum = 0;
-        for(int n: list){
-            System.out.println(n);
-            sum += n;
+        // List<Integer> list = new ArrayList<>();
+        // for(int i= 0; i<10; i++){
+        //     list.add(i);
+        // }
+        // for(int n : numbers){
+        //     list.remove(Integer.valueOf(n));
+        // }
+        // int sum = 0;
+        // for(int n: list){
+        //     System.out.println(n);
+        //     sum += n;
+        // }
+        int sum = 45;
+        for(int n:numbers){
+            sum -= n;
         }
 
         return sum;
